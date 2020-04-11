6bbc0a1064f267bc4b9006dc0c9adf13d56f54e2
Yonus Bakhshi <yonus.bakhshi@gmail.com>
2020-04-11 20:26:41 +0200

    baseurl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,7 @@ function App() {
   return (
       <div className ="Site">
         <ToastContainer/>
-        <Router history ={history}>
+        <Router  basename= {process.env.PUBLIC_URL}>
           <Header/>     
           <div className ="Site-content">
                 <Switch>
